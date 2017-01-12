a0766602da2311c90095ff4f4df0d171d76dad0e
Xia_T <xiacz_bj16398@163.com>
2017-01-12 15:54:48 +0800

    新建luanshengMobile

---
+++ app/src/main/java/danceup/luansheng01/LuanShengMobile.java
@@ -0,0 +1,20 @@
+package danceup.luansheng01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xiachenzhi on 2017/1/12.
+ */
+
+
+//branch mobile端
+
+public class LuanShengMobile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
